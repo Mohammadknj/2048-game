322e0882d70a5451598a7c62d8faedc84623b84a
Mohammad <mohammad8mohammadi4@gmail.com>
2024-08-28 21:09:06 +0330

    working ...

--- index.js
+++ index.js
@@ -6,34 +6,36 @@ function check_full_houses(matrix) {
   for (let i = 0; i < 4; i++) {
     for (let j = 0; j < 4; j++) {
       if (matrix[i][j].innerHTML == "") {
-        console.log('found empty house')
         return false;
       }
     }
   }
   return true;
 }
-function setRandNum(matrix) {console.log('not checked')
-console.log(matrix)
-  if (!check_full_houses(matrix)) {console.log('checked true')
+function setRandNum(matrix) {
+  if (!check_full_houses(matrix)) {
     while (true) {
       let row = Math.floor(Math.random() * 4);
-      let col = Math.floor(Math.random() * 4);console.log('before 0 text')
+      let col = Math.floor(Math.random() * 4);
       if (matrix[row][col].innerHTML == "") {
-        console.log(matrix[row][col])
-        console.log('after 0 text')
         let random = Math.floor(Math.random() * 10);
-        if(random==7){
-            matrix[row][col].innerHTML = 4
-        }else matrix[row][col].textContent = 2
-        console.log('wanna return')
+        if (random == 7) {
+          matrix[row][col].innerHTML = 4;
+          matrix[row][col].style.backgroundColor='#eee1c9'
+          matrix[row][col].style.color='#776e65'
+        } else {
+            matrix[row][col].textContent = 2;
+            matrix[row][col].style.backgroundColor='#eee4da'
+          matrix[row][col].style.color='#776e65'
+        }
         return;
       }
     }
   }
 }
-let Matrix = [[], [], [], []];
+
 startButton.addEventListener("click", () => {
+  let Matrix = [[], [], [], []];
   if (!started) {
     started = true;
     for (let i = 0; i < 4; i++) {
@@ -41,25 +43,7 @@ startButton.addEventListener("click", () => {
         Matrix[i][j] = document.getElementById(`house ${i * 4 + j + 1}`);
       }
     }
-    console.log(document.getElementById(`house ${1}`).textContent);
-    console.log('start')
     setRandNum(Matrix);
-    console.log('finish')
-    
-  } else //location.reload();
-{
-    for (let i = 0; i < 4; i++) {
-        for (let j = 0; j < 4; j++) {
-            if(Matrix[i][j].innerHTML!=''){
-                for(let k=0;k<4;k++){
-                    Matrix[i][k].style.backgroundColor='red'
-
-                }                
-                Matrix[i][3].textContent = Matrix[i][j].textContent
-                Matrix[i][j].textContent=''
-                break
-            }
-        }
-    }
-}
+    setRandNum(Matrix)
+  } else location.reload();
 });
